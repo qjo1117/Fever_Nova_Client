52c8c3a0f09a2f5c4dae31721705775a351d748f
Parkmuk <qjo1117@naver.com>
2022-07-27 11:43:19 +0900

    22-07-27 박정욱 Update

---
+++ Assets/Scripts/Content/SceneSpawnPoint/CollisionRespawn.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionRespawn : MonoBehaviour
+{
+	private void OnTriggerEnter(Collider other)
+	{
+		int l_layer = other.gameObject.layer;
+
+		// 플레이어일 경우
+		if (l_layer == (int)Define.Layer.Player) {
+			other.transform.position = Managers.Game.Respawn.GetRespawnPoint(Managers.Game.RespawnIndex);
+		}
+		
+	}
+}
