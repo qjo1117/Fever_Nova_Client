87b1af5dc55c76c6be0225699b393d1f493be954
lapisy <cdn06368@gmail.com>
2022-07-29 19:30:43 +0900

    22/07/29 이스터 에그 수정
    궁금한 사람은 게임플레이 선택 UI (싱글 /멀티 선택)에서 ESC 키를 눌러보세요

--- Assets/Scripts/UI/Popup/UI_GamePlaySelect.cs
+++ Assets/Scripts/UI/Popup/UI_GamePlaySelect.cs
@@ -5,9 +5,16 @@ using UnityEngine.UI;
 
 public class UI_GamePlaySelect : UI_Popup
 {
+    enum GameObjects
+    {
+        EasterEgg
+    }
+
     enum Images
     {
-        Background
+        Background,
+        NoGlassesChan,
+        Glasses
     }
 
     enum Buttons
@@ -24,10 +31,13 @@ public class UI_GamePlaySelect : UI_Popup
         MultiPlayButtonText
     }
 
+    private Coroutine m_runningCoroutine;
+
     public override void Init()
     {
         base.Init();
 
+        Bind<GameObject>(typeof(GameObjects));
         Bind<Image>(typeof(Images));
         Bind<Button>(typeof(Buttons));
         Bind<Text>(typeof(Texts));
@@ -35,6 +45,17 @@ public class UI_GamePlaySelect : UI_Popup
         Get<Button>((int)Buttons.BackButton).onClick.AddListener(() => { ClosePopupUI(); });
         Get<Button>((int)Buttons.SinglePlayButton).onClick.AddListener(SinglePlayClick);
         Get<Button>((int)Buttons.SinglePlayButton).onClick.AddListener(MultiPlayClick);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(m_runningCoroutine == null)
+            {
+                m_runningCoroutine =  StartCoroutine(DepressedChanMove(true));
+            }
+        }
     }
 
     // 싱글플레이 버튼 클릭시
@@ -48,5 +69,108 @@ public class UI_GamePlaySelect : UI_Popup
     {
 
     }
+
+    #region EasterEgg
+
+    IEnumerator DepressedChanMove(bool _isUp)
+    {
+        Vector3         l_prevPosition;
+        float           l_moveY = 2.0f;
+        Transform   l_easterEggTrans = Get<GameObject>((int)GameObjects.EasterEgg).GetComponent<Transform>();
+
+        while (true)
+        {
+            l_prevPosition = l_easterEggTrans.localPosition;
+            if (_isUp)
+            {
+                l_easterEggTrans.localPosition = new Vector3(l_prevPosition.x, l_prevPosition.y + l_moveY, l_prevPosition.z);
+
+                if (l_easterEggTrans.localPosition.y >= 0)
+                {
+                    m_runningCoroutine = StartCoroutine(GlassesMove(false));
+                    yield break;
+                }
+            }
+            else
+            {
+                l_easterEggTrans.localPosition = new Vector3(l_prevPosition.x, l_prevPosition.y - l_moveY, l_prevPosition.z);
+
+                if (l_easterEggTrans.localPosition.y <= -1130)
+                {
+                    m_runningCoroutine = null;
+                    yield break;
+                }
+            }
+
+            yield return null;
+        }
+    }
+
+    IEnumerator ImageRotate()
+    {
+        int     l_rotateCount = 0;
+        float   l_angle = 0.0f;
+        float   l_angleIncrease = 2.0f;
+
+        while(true)
+        {
+            Get<Image>((int)Images.Glasses).GetComponent<RectTransform>().Rotate(Vector3.forward, l_angleIncrease);
+            Get<Image>((int)Images.NoGlassesChan).GetComponent<RectTransform>().Rotate(Vector3.forward, -l_angleIncrease);
+
+            l_angle += l_angleIncrease;
+
+            if(l_angle >= 360.0f)
+            {
+                l_angle = 0.0f;
+                l_rotateCount++;
+
+                if(l_rotateCount >=2)
+                {
+                    m_runningCoroutine = StartCoroutine(GlassesMove(true));
+                    yield break;
+                }
+            }
+
+            yield return null;
+        }
+    }
+
+    IEnumerator GlassesMove(bool _isInMove)
+    {
+        RectTransform l_glassesRectTrans = Get<Image>((int)Images.Glasses).GetComponent<RectTransform>();
+        Vector3 l_initPosition = l_glassesRectTrans.localPosition;
+
+        Vector2 l_moveVec = new Vector2(1.0f, -1.0f);
+        Vector3 l_prevPosition;
+
+        while(true)
+        {
+            l_prevPosition = l_glassesRectTrans.localPosition;
+            if (_isInMove)
+            {
+                l_glassesRectTrans.localPosition = new Vector3(l_prevPosition.x - l_moveVec.x, l_prevPosition.y - l_moveVec.y, l_prevPosition.z);
+
+                if (l_glassesRectTrans.localPosition.y - l_initPosition.y >= 100)
+                {
+                    m_runningCoroutine = StartCoroutine(DepressedChanMove(false));
+                    yield break;
+                }
+            }
+            else
+            {
+                l_glassesRectTrans.localPosition = new Vector3(l_prevPosition.x + l_moveVec.x, l_prevPosition.y + l_moveVec.y, l_prevPosition.z);
+
+                if (l_initPosition.y - l_glassesRectTrans.localPosition.y >= 100)
+                {
+                    m_runningCoroutine = StartCoroutine(ImageRotate());
+                    yield break;
+                }
+            }
+
+            yield return null;
+        }   
+    }
+
+    #endregion
 
 }
